a21571e51b0a53b1a1733efd5a1c83505002ad5f
Rasmus van Guido <greencloud@users.noreply.github.com>
2016-09-10 13:56:33 +0800

    Update cab-bersyon.c

--- source/cab-bersyon.c
+++ source/cab-bersyon.c
@@ -1,26 +1,26 @@
 /*
  * Copyright (C) 2016 Rasmus van Guido <goldenfinch.net@zoho.com>
  *
- * This file is part of SCapt.
+ * This file is part of clAPT.
  *
- * SCapt is free software: you can redistribute it and/or modify
+ * clAPT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SCapt is distributed in the hope that it will be useful,
+ * clAPT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SCapt. If not, see <http://www.gnu.org/licenses/>.
+ * along with clAPT. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef scapt
+#ifndef clapt
 #include "clapt.h"
 #endif
 
